723a453685abc98bdc96c94f410e9f29d5c6893d
Andrew Funderburgh <andrewfunderburgh@gmail.com>
2016-08-14 01:22:00 -0500

    Updated the Roller class flow and made it more modularized

--- DnDStatRoller/Program.cs
+++ DnDStatRoller/Program.cs
@@ -14,7 +14,7 @@ namespace DnDStatRollerProgram
             string input = String.Empty;
 
             string welcomeMessage = "DnD 5th Edition Stat Generator";
-            string optionMessage = "-g [raw,std,] type exit to quit";
+            string optionMessage = "-g [raw,std,] -normalize" + Environment.NewLine + "Type exit, q, or quit to quit";
 
             Console.WriteLine(welcomeMessage);
             Console.WriteLine(optionMessage);
@@ -29,7 +29,7 @@ namespace DnDStatRollerProgram
                 switch (input)
                 {
                     case "1":
-                        int[] test = statGenerator.Generate(6, 6);
+                        List<int> test = statGenerator.RollStats(6, statGenerator.DefaultNormalize);
                         Console.WriteLine("Output:" + string.Join(",", test));
                         break;
                     case "exit":
@@ -43,19 +43,6 @@ namespace DnDStatRollerProgram
                 }
 
             }
-
-            
-            /*
-            Console.Write("Raw Roll:");
-
-            Console.Write(string.Join(",", arr) + Environment.NewLine);
-
-            arr.Remove(arr.Min());
-
-            Console.Write("Modified Roll:");
-            int count = 0;
-            arr.ForEach(x => { Console.Write(x); count += x; });
-            Console.WriteLine("Total:" + count.ToString());*/
         }
     }
 
@@ -64,29 +51,101 @@ namespace DnDStatRollerProgram
         public StatGenerator() 
         {
             randomObject = new Random(Guid.NewGuid().GetHashCode());
+            minimumAcceptableStatSum = 65;
+            minimumAcceptableIndividualStat = 9;
+            diceType = 6;
         }
 
         public StatGenerator(int seed)
         {
             randomObject = new Random(seed);
+            minimumAcceptableStatSum = 65;
+            minimumAcceptableIndividualStat = 9;
+            diceType = 6;
         }
 
-        private Random randomObject;
+        private int minimumAcceptableStatSum;
+        private int minimumAcceptableIndividualStat;
+        private int negativeModifierMaximum;
 
-        public int[] Generate(int numStats, int diceType)
+        private Random randomObject;
+        private int diceType = 6;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="numStats"></param>
+        /// <returns></returns>
+        public List<int> RollStats(int numStats, Func<List<List<int>>, List<int>> normalizationFunction)
         {
-            int[] stats = new int[numStats];
+            List<int> normalizedStats;
+            do
+            {
+                var stats = Generate(numStats);
+
+                normalizedStats = normalizationFunction(stats);
+            }
+            while (!Analyze(normalizedStats));
+
+            return normalizedStats;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="numStats"></param>
+        /// <param name="diceType"></param>
+        /// <returns></returns>
+        private List<List<int>> Generate(int numStats)
+        {
+            var stats = new List<List<int>>();
             for (int i = 0; i < numStats; i++)
             {
-                int[] tmp = new int[4];
-                tmp[0] = randomObject.Next(1, diceType);
-                tmp[1] = randomObject.Next(1, diceType);
-                tmp[2] = randomObject.Next(1, diceType);
-                tmp[3] = randomObject.Next(1, diceType);
+                var tmp = new List<int>();
+                tmp.Add(randomObject.Next(1, diceType));
+                tmp.Add(randomObject.Next(1, diceType));
+                tmp.Add(randomObject.Next(1, diceType));
 
-                stats[i] = tmp.OrderBy(x => x).Skip(1).Sum();
+                stats.Add(tmp);
             }
             return stats;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stats"></param>
+        /// <returns></returns>
+        private bool Analyze(List<int> stats)
+        {
+            if (stats.Sum() < minimumAcceptableStatSum)
+                return false;
+
+            if (stats.Min() < minimumAcceptableIndividualStat)
+                return false;
+
+            //check for # of negative stats (8 or lower)
+
+            return true;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stats"></param>
+        /// <returns></returns>
+        public List<int> DefaultNormalize(List<List<int>> stats)
+        {
+            var newStats = new List<int>();
+
+            foreach (List<int> stat in stats)
+            {
+                stat.Add(randomObject.Next(1, diceType));
+                newStats.Add(stat.OrderBy(x => x).Skip(1).Sum());
+            }
+
+            return newStats;
         }
     }
 }
